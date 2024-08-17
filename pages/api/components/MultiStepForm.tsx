158c987fb4d7f9bfb255ee351a3e8ccd480d5802
josill <josill@taltech.ee>
2024-08-17 11:31:34 +0300

    step 2

--- pages/api/components/MultiStepForm.tsx
+++ pages/api/components/MultiStepForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import { UseFormReturn } from "react-hook-form";
 import { ZodObject } from "zod";
 
@@ -7,12 +7,54 @@ interface MultiStepFormProps {
   schema: ZodObject<any>;
   /* The useForm hook return object */
   methods: UseFormReturn<any>;
+  /* The steps of the form, where the name of the step matches the one in the schema */
+  steps: { name: string; children: ReactNode }[];
   /* The function to call when the form is submitted */
   onSubmit: (data: any) => void;
 }
 
-function MultiStepForm({ schema, methods, onSubmit }: MultiStepFormProps) {
-  return <></>;
+function MultiStepForm({
+  schema,
+  methods,
+  steps,
+  onSubmit,
+}: MultiStepFormProps) {
+  const schemaKeys: string[] = schema.keyof()._def.values;
+  const numberOfFields = schemaKeys.length;
+  if (numberOfFields !== steps.length)
+    throw new Error("Amount of steps and fields in schema do not match");
+
+  const [currentStep, setCurrentStep] = useState(0);
+  const isLastStep = currentStep === steps.length - 1;
+
+  const handleBack = () => {
+    if (currentStep > 0) {
+      const newStep = currentStep - 1;
+      setCurrentStep(newStep);
+    }
+  };
+
+  const handleNext = () => {
+    const parse = schema.safeParse(methods.getValues());
+    const error = parse.error?.issues.find(
+      (i) => i.path[0] === steps[currentStep].name
+    );
+    if (!isLastStep && !error) {
+      const newStep = currentStep + 1;
+      setCurrentStep(newStep);
+    } else {
+      methods.handleSubmit(onSubmit)();
+    }
+  };
+
+  return (
+    <div className="w-full">
+      {steps.map(
+        (step, index) =>
+          index === currentStep && <div key={index}>{step.children}</div>
+      )}
+    </div>
+  );
 }
 
 export default MultiStepForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,36 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import MultiStepForm from "./api/components/MultiStepForm";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const inter = Inter({ subsets: ["latin"] });
+const schema = z.object({
+  personal: z.object({
+    name: z.string(),
+    email: z.string().email(),
+  }),
+  address: z.object({
+    street: z.string(),
+    city: z.string(),
+    state: z.string(),
+    zip: z.string(),
+  }),
+});
+
+type FormValues = z.infer<typeof schema>;
 
 export default function Home() {
-  return <div></div>;
+  const methods = useForm<FormValues>({
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <MultiStepForm
+      schema={schema}
+      methods={methods}
+      steps={[]}
+      onSubmit={() => {}}
+    />
+  );
 }
